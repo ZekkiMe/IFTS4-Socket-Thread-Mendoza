ad4cb1641cf4b6b6f335c3a2feb5571d081751b2
Ezequiel Mendoza <zekkimendoza@gmail.com>
2025-06-27 14:09:15 -0300

    Final..?

--- client.py
+++ client.py
@@ -1,22 +1,59 @@
 import socket
-
+import threading
+import os
 HOST = "127.0.0.1"  # ip del servidor al cual me voy a conectar
 PORT = 40123        # puerto de conexion
+nombre = None
+# Esta funcion recive los mensajes del servidor lo decodifica y evalua si esta vacio, caso contrario lo muestra en pantalla 
+def recibir():
+    while True:
+        try:
+            mensaje = cliente.recv(1024).decode("utf-8")
+            print(mensaje)
 
+        except Exception as e:
+            print(e)
+            cliente.close()
+            break
+
+# Esta funcion solicita ingresar un mensaje y luego lo envia codificado en caso de ser "/exit, "
+def enviar():
+    while True:
+        mensaje = input("")
+        if mensaje.lower() == "/clear":
+            os.system('cls')
+        else:
+            try:
+                if mensaje.lower() == "/exit":
+                    print("Cerrando sesión...")
+                    cliente.send(mensaje.encode("utf-8"))
+                    cliente.close()
+                    break
+                elif mensaje.lower() == "/login":
+                    nombre = input("Ingrese el nombre: ")
+                    if nombre:
+                        cliente.send(f"{mensaje}{nombre}".encode("utf-8"))
+                    else:
+                        print("\n [Server] El nombre de usuario no puede estar vacío.")
+                elif mensaje.lower() == "/send":
+                    usuario = input("Ingrese el Nombre del destinatario: ")
+                    mensaje_privado = input("Ingrese su mensaje: ")
+                    cliente.send(f"{mensaje} {usuario} {mensaje_privado}".encode("utf-8"))
+                elif mensaje.lower() == "/sendall":
+                    mensaje_all = input("ingrese su mensaje: ")
+                    cliente.send(f"{mensaje} {mensaje_all}".encode("utf-8"))
+                
+                else:
+                    cliente.send(mensaje.encode("utf-8"))
+            except Exception as e:
+                print(e)
+                cliente.close()
 
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect((HOST, PORT))
 
-def main():
-    data = cliente.recv(1024)
-    print(data.decode("utf-8"))
+recibir = threading.Thread(target=recibir)
+recibir.start()
 
-    msj = ""
-    while msj != "/exit":
-        msj = input("Tu >> ")
-        cliente.send(msj.encode("utf-8"))
-        msj_server = cliente.recv(1024).decode("utf-8")
-        print("Server >> " + msj_server)
-    cliente.close()
-
-main()
+enviar = threading.Thread(target=enviar)
+enviar.start()
--- server.py
+++ server.py
@@ -1,91 +1,139 @@
 import socket
+import threading
 
 HOST = "127.0.0.1" # direccion de loopback
 PORT = 40123        # usar puertos entre 1023 y 65535
 
-help_msj = "\n\n/login \t\tPara ingresar\n/send \t\tEnviar mensaje a otro usuario\n/sendall  \tEnviar mensaje a todos los usuarios conectados\n/show  \t\tMostrar usuarios conectados\n/help \t\tVer los comandos\n/exit \t\tSalir\n"
-primer_msj = f"Hola Bienvenido al Servidor!!{help_msj}"
+HELP_MSJ = '''
+/help                                   Muestra las funciones
+/show                                   Muestra los usuarios conectados
+/exit                                   Te desconecta del servidor
+/login <nombre>                         Ingresa a al servicio de chat
+/sendall <Mensaje>                      Envia un mensaje a todos los usuario conectados
+/send <nombre_destinatario> <mensaje>   Envia un mensaje directo a el usuario seleccionado
+/clear                                  Limpia la terminal
+'''
+PRIMER_MSJ = f"Hola Bienvenido al Servidor!!\n{HELP_MSJ}"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
-server.listen()
+server.listen(5)
 
-usuario = ""
+clientes =[]
+nombres = []
 
-def login(client):
-    global usuario
-    while usuario == "":
-        client.send("Envie su nombre".encode("utf-8"))
-        data = client.recv(1024)
+def broadcast(mensaje, cliente_enviador):
+    for cliente in clientes:
+        if cliente != cliente_enviador:
+            try:
+                cliente.send(mensaje)
+            except:
+                if cliente in clientes:
+                    idx = clientes.index(cliente)
+                    if idx < len(nombres):
+                        desconectado = nombres[idx]
+                        print(f"[Server] {desconectado} desconectado (error al enviar).")
+                        clientes.remove(cliente)
+                        nombres.remove(desconectado)
+                    else:
+                        clientes.remove(cliente)
+                else:
+                    pass
 
-        if not data:
-            client.send("Campo vacio \ningrese nuevamente o /exit para salir".encode("utf-8"))
-        elif data == "/exit":
-            break
-        else:
-            usuario = data.decode("utf-8")
-            client.send("Nombre Cargado".encode("utf-8"))
-
-def exit(client, ip):
-    global usuario
-    print(f"Se desconectó > {usuario} > {ip} ")
-    client.send("Desconectando del Servidor..".encode("utf-8"))
-    client.close()
-    usuario = ""
-
-def help(client, ip):
-    global usuario
-    if usuario != "":
-        print(f"{usuario} Solcitó /help")
-    else:
-        print(f"{ip} Solcitó /help")
-    client.send(help_msj.encode("utf-8"))
-
-def main(): 
-    global usuario
-    print("esperando conexion..")
-    conexion_cliente, direccion = server.accept()
-    conexion_cliente.send(primer_msj.encode("utf-8"))
-    print(f"Conexion desde: {direccion}")
-
+def handle_client(client, addr):
+    nombre_cliente = None
 
     while True:
-        data = conexion_cliente.recv(1024)
+        try:
+            mensaje = client.recv(1024)
+            msj = mensaje.decode("utf-8")
 
-        if not data:
-            print(f"Se desconectó el cliente {direccion}")
-            conexion_cliente.close()
-            usuario = ""
-            break
-            
-        mensaje_cliente = data.decode("utf-8")
-        match mensaje_cliente:
-            case "/login":
-                login(conexion_cliente)
-                if usuario != "":
-                    print(f"{direccion} > {usuario} ")
-            case "/exit":
-                exit(conexion_cliente, direccion)
+            if msj.lower() == "/exit":
                 break
-            case "/help":
-                help(conexion_cliente, direccion)
-            case _:
-                if usuario != "":
-                    print(f"{usuario} >> " + mensaje_cliente)
+
+            elif msj.lower() == "/help":
+                client.send(HELP_MSJ.encode("utf-8"))
+
+            elif msj.lower() == "/show":
+                mensaje = "\nUsuarios conectados:"
+                for nombre in nombres:
+                    mensaje+= f"\n\t{nombre}"
+                client.send(f"{mensaje}\n".encode("utf-8"))
+
+            elif msj.lower().startswith("/login"):
+                data = msj[len("/login"):].strip()
+                if data in nombres: 
+                    client.send("\n [Server] Ese nombre ya se encuentra en uso".encode("utf-8"))
                 else:
-                    print("Cliente >> " + mensaje_cliente)
-                msj_server = "Recibido"
-                conexion_cliente.send(msj_server.encode("utf-8"))
+                    nombres.append(data)
+                    clientes.append(client)
+                    client.send("\n [Server] Recibido. Para enviar al chat Grupal ingresa -> /sendall <mensaje>\n\tPara un mensaje directo -> /send <nombre_destinatario> <mensaje>".encode("utf-8"))
+                    nombre_cliente = data
+                    broadcast(f"{data} se ha logeado".encode("utf-8"), client)
 
+            elif msj.lower().startswith("/sendall"):
+                if nombre_cliente is None:
+                    client.send("\n [Server] Necesitas logearte para enviar mensajes.".encode("utf-8"))
+                    continue
+                else:
+                    data = msj[len("/sendall"):].strip()
+                    broadcast(f"{nombre_cliente}: {data}".encode("utf-8"), client)
 
-                
+            else:
+                if msj.lower().startswith("/send"):
+                    if nombre_cliente is None:
+                        client.send("\n [Server] Necesitas logearte para enviar mensajes.".encode("utf-8"))
+                        continue
+                    else:
+                        data = msj.split(' ', 2)
+                        if len(data) < 3:
+                            client.send("\n [Server] Formato incorrecto para /send. Usa: /send <nombre> <mensaje>".encode("utf-8"))
+                            continue
+                        d_nombre = data[1]
+                        d_mensaje = data[2]
+                        if d_nombre == nombre_cliente:
+                            client.send("\n [Server] No te puedes enviar un mensaje a ti mismo".encode("utf-8"))
+                            continue
+                        else:    
+                            if d_nombre in nombres:
+                                index = clientes[nombres.index(d_nombre)]
+                                index.send(f"[DIRECTO]{nombre_cliente}: {d_mensaje}".encode("utf-8"))
+                                client.send(f"\n [Server] Mensaje directo enviado a {d_nombre}.".encode("utf-8"))
+                            else:
+                                client.send("\n [Server] No se encuetra ese usuario".encode("utf-8"))
+                else:
+                    if nombre_cliente is None:
+                        client.send("\n [Server] Necesitas iniciar sesión con /login <nombre> para chatear.\n".encode("utf-8"))
+                        print(f"Mensaje no logeado de {addr[0]}: {msj}")
+                    else:
+                        client.send("\n [Server] Comando no reconocido. Usa /help para ver los comandos.\n".encode("utf-8"))
+                        print(f"Comando desconocido de {nombre_cliente}: {msj}")
 
-# print("esperando conexion..")
-try:
-    main()
-except KeyboardInterrupt:
-    print("\nServidor apagándose...")
-    server.close()
-except Exception as e:
-    print(f"Ha ocurrido un error inesperado en el servidor: {e}")
-    server.close()
+        except:
+            index = clientes.index(client)
+            nombre = nombres[index]
+            broadcast(f"\n [Server] <{nombre}> Se Desconectó".encode('utf-8'), client)
+            clientes.remove(client)
+            nombres.remove(nombre)
+            client.close()
+            break
+
+def receive():
+    try:
+        while True:
+            client, addr = server.accept()
+            print(f"Accepted connection from {addr[0]}:{addr[1]}")
+            client.send(PRIMER_MSJ.encode("utf-8"))
+
+            thread = threading.Thread(target=handle_client, args=(client,addr,))
+            thread.start()
+    except KeyboardInterrupt: # Cuando se presiona Ctrl+C
+        print("\nServidor apagándose...")
+    except Exception as e: # Para otros errores inesperados
+        print(f"Error inesperado en receive: {e}")
+    finally: # Por ultimo Cierra el Server
+        server.close()
+        print("Servidor cerrado.")
+
+print("Servidor esperando conexiones.")
+receive()
